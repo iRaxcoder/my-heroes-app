705c2e26661979a24d22698a99c2cf8ce4ae044b
Randall <randms07@hotmail.com>
2022-09-19 21:39:57 -0600

    nase name changed

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/heroes">
       <HeroesApp></HeroesApp>
     </BrowserRouter>
   </React.StrictMode>
