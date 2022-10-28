0e6b5187e7ccd605e20e4ed003e8703d2b615c0e
Randall <randms07@hotmail.com>
2022-10-28 10:25:53 -0600

    feat: add .env file for credentials

--- src/helper/getData.js
+++ src/helper/getData.js
@@ -1,9 +1,9 @@
 const BASE_URL = "https://gateway.marvel.com/v1/public/";
-const ACCESS_PARAMS =
-  "ts=1&apikey=1d7dd5a031787db42d3e0688ef408b62&hash=c496afb27443f7cf3d6c97fe48448930&limit=30";
+const ACCESS_PARAMS = import.meta.env.VITE_API_KEY;
 
 export const getDataFromUrl = async (query) => {
   const url = BASE_URL + query + ACCESS_PARAMS;
+  console.log(ACCESS_PARAMS);
 
   const resp = await fetch(url);
 
